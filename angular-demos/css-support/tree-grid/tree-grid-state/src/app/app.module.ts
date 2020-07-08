df0555519e85e51795e1e8e3c161031b2aff602e
tfsbuild <tfsbuild@infragistics.com>
2020-07-08 13:39:32 +0300

    Commit submodule changes angular-samples-cd_2020.7.8.3

---
+++ angular-demos/css-support/tree-grid/tree-grid-state/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppComponent } from "./app.component";
+import { IgxPreventDocumentScrollModule } from "./directives/prevent-scroll.directive";
+import { IgxTreeGridModule } from "igniteui-angular";
+import { TGridSaveStateComponent } from "./tree-grid-save-state/tGrid-state.component";
+import { TGridAboutComponent } from "./tree-grid-save-state/about.component";
+
+
+
+@NgModule({
+  bootstrap: [AppComponent],
+  declarations: [
+    AppComponent,
+		TGridSaveStateComponent,
+		TGridAboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+		IgxPreventDocumentScrollModule,
+		IgxTreeGridModule,
+		RouterModule.forRoot([{component: TGridAboutComponent, path: 'tree-grid-about'},{component: TGridSaveStateComponent, path: 'tree-grid-state'},{ path: '', redirectTo: '/tree-grid-state', pathMatch: 'full' }])]
+  ],
+  providers: [],
+  entryComponents: [],
+  schemas: []
+})
+export class AppModule {}
