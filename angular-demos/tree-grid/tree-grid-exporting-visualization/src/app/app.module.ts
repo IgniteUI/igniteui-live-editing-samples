6a71d3a6ceee0e84816676119bbc448c48268707
tfsbuild <tfsbuild@infragistics.com>
2020-09-08 15:56:28 +0300

    Commit submodule changes angular-samples-cd_2020.9.8.1

---
+++ angular-demos/tree-grid/tree-grid-exporting-visualization/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppComponent } from "./app.component";
+import { IgxPreventDocumentScrollModule } from "./directives/prevent-scroll.directive";
+import { 
+	IgxTreeGridModule,
+	IgxToggleModule,
+	IgxExcelExporterService,
+	IgxCsvExporterService
+ } from "igniteui-angular";
+import { TreeGridExportVisualizationComponent } from "./tree-grid-export-visualization/tree-grid-export-visualization.component";
+
+
+
+@NgModule({
+  bootstrap: [AppComponent],
+  declarations: [
+    AppComponent,
+		TreeGridExportVisualizationComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+		IgxPreventDocumentScrollModule,
+		IgxTreeGridModule,
+		IgxToggleModule
+  ],
+  providers: [
+		IgxExcelExporterService,
+		IgxCsvExporterService
+	],
+  entryComponents: [],
+  schemas: []
+})
+export class AppModule {}
