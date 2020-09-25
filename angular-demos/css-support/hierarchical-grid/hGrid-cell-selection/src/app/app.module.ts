a239669aa2a52d781e54a63f85a54898f046a561
tfsbuild <tfsbuild@infragistics.com>
2020-09-25 16:43:08 +0300

    Commit submodule changes angular-samples-cd_2020.9.25.1

---
+++ angular-demos/css-support/hierarchical-grid/hGrid-cell-selection/src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppComponent } from "./app.component";
+import { 
+	IgxHierarchicalGridModule,
+	IgxSnackbarModule,
+	IgxButtonModule,
+	IgxButtonGroupModule,
+	IgxIconModule
+ } from "igniteui-angular";
+import { HierarchicalGridCellSelectionComponent } from "./hierarchical-grid/hierarchical-grid-cellSelection-sample/hierarchical-grid-cellSelection.component";
+import { IgxPreventDocumentScrollModule } from "./directives/prevent-scroll.directive";
+
+
+
+@NgModule({
+  bootstrap: [AppComponent],
+  declarations: [
+    AppComponent,
+		HierarchicalGridCellSelectionComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+		IgxPreventDocumentScrollModule,
+		IgxHierarchicalGridModule,
+		IgxSnackbarModule,
+		IgxButtonModule,
+		IgxButtonGroupModule,
+		IgxIconModule
+  ],
+  providers: [],
+  entryComponents: [],
+  schemas: []
+})
+export class AppModule {}
