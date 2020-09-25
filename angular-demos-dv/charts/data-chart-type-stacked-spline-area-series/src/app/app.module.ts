c31bd1f6d4d733b5c7a4850f1a168aa1c159802d
ESShared <EServices@infragistics.com>
2020-09-25 20:09:32 +0300

    Commit submodule changes AngularSamplesDV_2020.9.25.4

---
+++ angular-demos-dv/charts/data-chart-type-stacked-spline-area-series/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppComponent } from "./app.component";
+
+@NgModule({
+  bootstrap: [AppComponent],
+  declarations: [
+    AppComponent,
+		DataChartTypeStackedSplineAreaSeriesComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+		IgxDataChartCoreModule,
+		IgxDataChartCategoryModule,
+		IgxDataChartInteractivityModule,
+		IgxLegendModule
+  ],
+  providers: [SharedData],
+  entryComponents: []
+})
+export class AppModule {}
---
+++ angular-demos-dv/css-support/charts/data-chart-type-stacked-100-spline-area-series/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppComponent } from "./app.component";
+
+@NgModule({
+  bootstrap: [AppComponent],
+  declarations: [
+    AppComponent,
+		DataChartTypeStacked100SplineAreaSeriesComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+		IgxDataChartCoreModule,
+		IgxDataChartCategoryModule,
+		IgxDataChartInteractivityModule,
+		IgxLegendModule
+  ],
+  providers: [SharedData],
+  entryComponents: []
+})
+export class AppModule {}
