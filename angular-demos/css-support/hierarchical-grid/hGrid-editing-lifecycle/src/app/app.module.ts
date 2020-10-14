c5e53712543a351175a4b3b77d73800912518cb6
tfsbuild <tfsbuild@infragistics.com>
2020-10-14 14:18:08 +0300

    Commit submodule changes angular-samples-cd_2020.10.14.1

---
+++ angular-demos/css-support/hierarchical-grid/hGrid-editing-lifecycle/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppComponent } from "./app.component";
+import { 
+	IgxHierarchicalGridModule,
+	IgxSwitchModule
+ } from "igniteui-angular";
+import { IgxPreventDocumentScrollModule } from "./directives/prevent-scroll.directive";
+import { HGridEditingLifecycleComponent } from "./hierarchical-grid/hgrid-editing-lifecycle-sample/hgrid-editing-lifecycle.component";
+
+
+
+@NgModule({
+  bootstrap: [AppComponent],
+  declarations: [
+    AppComponent,
+		HGridEditingLifecycleComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+		IgxPreventDocumentScrollModule,
+		IgxHierarchicalGridModule,
+		IgxSwitchModule
+  ],
+  providers: [],
+  entryComponents: [],
+  schemas: []
+})
+export class AppModule {}
