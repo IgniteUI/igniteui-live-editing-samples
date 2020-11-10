cd11569a35b4b410d462cf177c26543677c199db
ESShared <EServices@infragistics.com>
2020-11-10 02:13:54 +0200

    Commit submodule changes AngularSamplesDV_2020.11.9.4

---
+++ angular-demos-dv/maps/geo-map-marker-layouts/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppComponent } from "./app.component";
+
+@NgModule({
+  bootstrap: [AppComponent],
+  declarations: [
+    AppComponent,
+		MapMarkerLayoutsComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+		IgxGeographicMapModule
+  ],
+  providers: [],
+  entryComponents: []
+})
+export class AppModule {}
