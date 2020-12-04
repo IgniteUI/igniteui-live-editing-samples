e48b3172b9796e5ff168b2064a7fc07555b359d4
tfsbuild <tfsbuild@infragistics.com>
2020-12-04 14:33:45 +0200

    Commit submodule changes angular-samples-cd_2020.12.4.2

--- angular-demos/css-support/menus/navbar-custom-title/src/app/navbar-custom-title/navbar-custom-title.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-navbar-custom-title',
-    templateUrl: './navbar-custom-title.component.html',
-    styleUrls: ['./navbar-custom-title.component.css']
-})
-export class NavbarCustomTitleComponent { }
