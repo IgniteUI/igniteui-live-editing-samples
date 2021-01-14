6106ba8f6d34390b0fe0e214acbf519129a29d41
tfsbuild <tfsbuild@infragistics.com>
2021-01-14 14:05:55 +0200

    Commit submodule changes angular-samples-cd_2021.1.14.1

---
+++ angular-demos/data-entries/buttons-roundness-sample/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppComponent } from "./app.component";
+import { 
+	IgxButtonModule,
+	IgxIconModule,
+	IgxRippleModule
+ } from "igniteui-angular";
+import { ButtonsRoundnessComponent } from "./buttons-roundness-sample/buttons-roundness-sample";
+
+
+
+@NgModule({
+  bootstrap: [AppComponent],
+  declarations: [
+	AppComponent,
+	ButtonsRoundnessComponent
+],
+  imports: [
+	BrowserModule,
+	BrowserAnimationsModule,
+	FormsModule,
+	IgxButtonModule,
+	IgxIconModule,
+	IgxRippleModule
+],
+  providers: [],
+  entryComponents: [],
+  schemas: []
+})
+export class AppModule {}
