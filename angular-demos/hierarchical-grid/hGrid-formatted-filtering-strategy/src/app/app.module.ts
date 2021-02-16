ab295b9008a852c200de533c429dc9209b3483b2
tfsbuild <tfsbuild@infragistics.com>
2021-02-16 10:51:51 -0500

    Commit submodule changes angular-samples-cd_2021.2.16.2

---
+++ angular-demos/hierarchical-grid/hGrid-formatted-filtering-strategy/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppComponent } from "./app.component";
+import { IgxHierarchicalGridModule } from "igniteui-angular";
+import { HGridFormattedFilteringStrategyComponent } from "./hierarchical-grid/hierarchical-grid-formatted-filtering-strategy/hierarchical-grid-formatted-filtering-strategy.component";
+import { IgxPreventDocumentScrollModule } from "./directives/prevent-scroll.directive";
+
+
+
+@NgModule({
+  bootstrap: [AppComponent],
+  declarations: [
+	AppComponent,
+	HGridFormattedFilteringStrategyComponent
+],
+  imports: [
+	BrowserModule,
+	BrowserAnimationsModule,
+	FormsModule,
+	IgxPreventDocumentScrollModule,
+	IgxHierarchicalGridModule
+],
+  providers: [],
+  entryComponents: [],
+  schemas: []
+})
+export class AppModule {}
