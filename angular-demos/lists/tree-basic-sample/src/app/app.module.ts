5a4204a2bd60e03facf0437103809c21b0bd7a5b
tfsbuild <tfsbuild@infragistics.com>
2021-04-26 11:37:42 +0300

    Commit submodule changes angular-samples-cd_2021.4.26.1

---
+++ angular-demos/lists/tree-basic-sample/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppComponent } from "./app.component";
+import { 
+	IgxTreeModule,
+	IgxIconModule
+ } from "igniteui-angular";
+import { TreeBasicSampleComponent } from "./lists/tree/tree-basic-sample/tree-basic-sample.component";
+
+
+
+@NgModule({
+  bootstrap: [AppComponent],
+  declarations: [
+	AppComponent,
+	TreeBasicSampleComponent
+],
+  imports: [
+	BrowserModule,
+	BrowserAnimationsModule,
+	FormsModule,
+	IgxTreeModule,
+	IgxIconModule
+],
+  providers: [],
+  entryComponents: [],
+  schemas: []
+})
+export class AppModule {}
