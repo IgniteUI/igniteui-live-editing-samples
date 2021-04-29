3fb98caf6e2bdcd0a7ed6b9d8db0c322ef516795
tfsbuild <tfsbuild@infragistics.com>
2021-04-29 10:38:16 +0300

    Commit submodule changes angular-samples-cd_2021.4.29.1

---
+++ angular-demos/tree-grid/treegrid-reusable-paginator/src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppComponent } from "./app.component";
+import { IgxPreventDocumentScrollModule } from "./directives/prevent-scroll.directive";
+import { 
+	IgxTreeGridModule,
+	IgxButtonModule,
+	IgxRippleModule,
+	IgxIconModule,
+	IgxSwitchModule
+ } from "igniteui-angular";
+import { TreeGridPagerSampleComponent } from "./tree-grid-pager-sample/tree-grid-pager-sample.component";
+
+
+
+@NgModule({
+  bootstrap: [AppComponent],
+  declarations: [
+	AppComponent,
+	TreeGridPagerSampleComponent
+],
+  imports: [
+	BrowserModule,
+	BrowserAnimationsModule,
+	FormsModule,
+	IgxPreventDocumentScrollModule,
+	IgxTreeGridModule,
+	IgxButtonModule,
+	IgxRippleModule,
+	IgxIconModule,
+	IgxSwitchModule
+],
+  providers: [],
+  entryComponents: [],
+  schemas: []
+})
+export class AppModule {}
