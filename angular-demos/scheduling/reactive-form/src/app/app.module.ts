b923f0cf7275f236aea2b7ad9acee4f4a6977672
tfsbuild <tfsbuild@infragistics.com>
2021-06-10 16:38:02 +0300

    Commit submodule changes angular-samples-cd_2021.6.10.1

---
+++ angular-demos/scheduling/reactive-form/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppComponent } from "./app.component";
+import { DateTimePickerRFSampleComponent } from "./scheduling/datepicker/reactive-form/reactive-form.component";
+
+
+
+@NgModule({
+  bootstrap: [AppComponent],
+  declarations: [
+	AppComponent,
+	DateTimePickerRFSampleComponent
+],
+  imports: [
+	BrowserModule,
+	BrowserAnimationsModule,
+	FormsModule,
+	IgxDatePickerModule,
+	IgxTimePickerModule
+],
+  providers: [],
+  entryComponents: [],
+  schemas: []
+})
+export class AppModule {}
