c719d140f5fb7a56bc1f95a504985e9041297e69
tfsbuild <tfsbuild@infragistics.com>
2021-07-06 09:13:23 +0300

    Commit submodule changes angular-samples-cd_2021.7.6.1

---
+++ angular-demos/hierarchical-grid/hierarchical-grid-multi-column-export/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppComponent } from "./app.component";
+import { IgxHierarchicalGridModule } from "igniteui-angular";
+import { HGridMultiColumnHeadersExportComponent } from "./hierarchical-grid/hierarchical-grid-multi-column-headers-export/hierarchical-grid-multi-column-export.component";
+import { IgxPreventDocumentScrollModule } from "./directives/prevent-scroll.directive";
+
+
+
+@NgModule({
+  bootstrap: [AppComponent],
+  declarations: [
+	AppComponent,
+	HGridMultiColumnHeadersExportComponent
+],
+  imports: [
+	BrowserModule,
+	BrowserAnimationsModule,
+	FormsModule,
+	IgxPreventDocumentScrollModule,
+	IgxHierarchicalGridModule
+],
+  providers: [],
+  entryComponents: [],
+  schemas: []
+})
+export class AppModule {}
