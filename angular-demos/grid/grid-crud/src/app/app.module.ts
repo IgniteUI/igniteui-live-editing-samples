f6c4c6b35e37dbc8a65655ecf4a37e92374cbbde
tfsbuild <tfsbuild@infragistics.com>
2021-08-13 16:28:29 +0300

    Commit submodule changes angular-samples-cd_2021.8.13.1

---
+++ angular-demos/grid/grid-crud/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppComponent } from "./app.component";
+import { CRUDSampleComponent } from "./grid/grid-crud-sample/crud-sample.component";
+import { 
+	IgxActionStripModule,
+	IgxGridModule
+ } from "igniteui-angular";
+import { HttpClientModule } from "@angular/common/http";
+import { IgxPreventDocumentScrollModule } from "./directives/prevent-scroll.directive";
+import { CRUDService } from "./services/crud.service";
+
+
+
+@NgModule({
+  bootstrap: [AppComponent],
+  declarations: [
+	AppComponent,
+	CRUDSampleComponent
+],
+  imports: [
+	BrowserModule,
+	BrowserAnimationsModule,
+	FormsModule,
+	IgxPreventDocumentScrollModule,
+	IgxGridModule,
+	IgxActionStripModule,
+	HttpClientModule
+],
+  providers: [CRUDService],
+  entryComponents: [],
+  schemas: []
+})
+export class AppModule {}
