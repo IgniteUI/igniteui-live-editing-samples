1f53578564974ddef775b2e4e02c5ea2b8f29cb4
dobromirts <dobromir.ts@gmail.com>
2021-10-14 09:13:43 +0300

    chore(*): restore hierarchical samples

---
+++ angular-demos/hierarchical-grid/hierarchical-grid-external-excel-style-filtering/src/app/hierarchical-grid/hierarchical-grid-external-excel-style-filtering/hierarchical-grid-external-excel-style-filtering.component.ts
@@ -0,0 +1,27 @@
+import { ChangeDetectionStrategy, Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { IgxHierarchicalGridComponent } from 'igniteui-angular';
+import { SINGERS } from '../../data/singersData';
+
+@Component({
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'app-hierarchical-grid-external-excel-style-filtering',
+    templateUrl: './hierarchical-grid-external-excel-style-filtering.component.html',
+    styleUrls: ['./hierarchical-grid-external-excel-style-filtering.component.scss']
+})
+export class HGridExternalExcelStyleFilteringComponent implements AfterViewInit{
+
+    @ViewChild('hierarchicalGrid', { read: IgxHierarchicalGridComponent, static: true })
+    public hgrid: IgxHierarchicalGridComponent;
+
+    public columns: any[];
+    public localdata: any[];
+
+    constructor() {
+        this.localdata = SINGERS;
+    }
+
+    public ngAfterViewInit() {
+        this.columns = this.hgrid.columns.filter(c => c.filterable);
+
+    }
+}
