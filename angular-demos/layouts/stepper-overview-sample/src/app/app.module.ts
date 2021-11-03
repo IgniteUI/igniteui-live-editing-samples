f25597f55678ad0f7e7affb2f5075e593aa22e59
tfsbuild <tfsbuild@infragistics.com>
2021-11-03 16:33:24 +0200

    Commit submodule changes angular-samples-cd_2021.11.3.1

---
+++ angular-demos/layouts/stepper-overview-sample/src/app/app.module.ts
@@ -0,0 +1,47 @@
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppComponent } from "./app.component";
+import { StepperOverviewSampleComponent } from "./stepper-overview-sample/stepper-overview-sample.component";
+import { 
+	IgxStepperModule,
+	IgxMaskModule,
+	IgxInputGroupModule,
+	IgxButtonModule,
+	IgxRadioModule,
+	IgxCardModule,
+	IgxCheckboxModule,
+	IgxSelectModule,
+	IgxIconModule,
+	IgxBadgeModule
+ } from "igniteui-angular";
+
+
+
+@NgModule({
+  bootstrap: [AppComponent],
+  declarations: [
+	AppComponent,
+	StepperOverviewSampleComponent
+],
+  imports: [
+	BrowserModule,
+	BrowserAnimationsModule,
+	FormsModule,
+	IgxStepperModule,
+	IgxMaskModule,
+	IgxInputGroupModule,
+	IgxButtonModule,
+	IgxRadioModule,
+	IgxCardModule,
+	IgxCheckboxModule,
+	IgxSelectModule,
+	IgxIconModule,
+	IgxBadgeModule
+],
+  providers: [],
+  entryComponents: [],
+  schemas: []
+})
+export class AppModule {}
---
+++ angular-demos/layouts/stepper-steptypes-sample/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppComponent } from "./app.component";
+import { StepperStepTypesSampleComponent } from "./stepper-steptypes-sample/stepper-steptypes-sample.component";
+import { 
+	IgxStepperModule,
+	IgxButtonGroupModule
+ } from "igniteui-angular";
+
+
+
+@NgModule({
+  bootstrap: [AppComponent],
+  declarations: [
+	AppComponent,
+	StepperStepTypesSampleComponent
+],
+  imports: [
+	BrowserModule,
+	BrowserAnimationsModule,
+	FormsModule,
+	IgxStepperModule,
+	IgxButtonGroupModule
+],
+  providers: [],
+  entryComponents: [],
+  schemas: []
+})
+export class AppModule {}
