1ed18bc02e32d739bb2f13f4fd97aae1328fb76b
tfsbuild <tfsbuild@infragistics.com>
2021-11-12 14:39:03 +0200

    Commit submodule changes angular-samples-cd_2021.11.12.1

---
+++ angular-demos/hierarchical-grid/hGrid-action-strip/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppComponent } from "./app.component";
+import { HGridActionStripSampleComponent } from "./hierarchical-grid/hierarchical-grid-action-strip/hierarchical-grid-action-strip-sample";
+import { 
+	IgxHierarchicalGridModule,
+	IgxDialogModule,
+	IgxButtonModule,
+	IgxActionStripModule
+ } from "igniteui-angular";
+import { IgxPreventDocumentScrollModule } from "./directives/prevent-scroll.directive";
+
+
+
+@NgModule({
+  bootstrap: [AppComponent],
+  declarations: [
+	AppComponent,
+	HGridActionStripSampleComponent
+],
+  imports: [
+	BrowserModule,
+	BrowserAnimationsModule,
+	FormsModule,
+	IgxPreventDocumentScrollModule,
+	IgxHierarchicalGridModule,
+	IgxDialogModule,
+	IgxButtonModule,
+	IgxActionStripModule
+],
+  providers: [],
+  entryComponents: [],
+  schemas: []
+})
+export class AppModule {}
