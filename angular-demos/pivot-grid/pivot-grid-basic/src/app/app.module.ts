50b309cac2270f1ed0068e41ecfb1c948e931a69
tfsbuild <tfsbuild@infragistics.com>
2022-02-15 11:38:23 +0200

    Commit submodule changes angular-samples-cd_2022.2.14.1

---
+++ angular-demos/pivot-grid/pivot-grid-basic/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppComponent } from "./app.component";
+import { IgxPivotGridModule } from "igniteui-angular";
+import { PivotGridBasicSampleComponent } from "./pivot-grid/pivot-basic-sample/pivot-grid-basic-sample.component";
+import { IgxPreventDocumentScrollModule } from "./directives/prevent-scroll.directive";
+
+
+
+@NgModule({
+  bootstrap: [AppComponent],
+  declarations: [
+	AppComponent,
+	PivotGridBasicSampleComponent
+],
+  imports: [
+	BrowserModule,
+	BrowserAnimationsModule,
+	FormsModule,
+	IgxPreventDocumentScrollModule,
+	IgxPivotGridModule
+],
+  providers: [],
+  entryComponents: [],
+  schemas: []
+})
+export class AppModule {}
