76e5259de179674d47926976c29b0878cc6dc80f
tfsbuild <tfsbuild@infragistics.com>
2022-08-19 11:03:59 -0400

    Commit submodule changes angular-samples-cd_2022.8.19.1

---
+++ angular-demos/grid/grid-columnAutoResizing-sample/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppComponent } from "./app.component";
+import { IgxGridModule } from "igniteui-angular";
+import { GridColumnAutosizingComponent } from "./grid/grid-column-autosizing/grid-column-autosizing.component";
+import { IgxPreventDocumentScrollModule } from "./directives/prevent-scroll.directive";
+
+
+
+@NgModule({
+  bootstrap: [AppComponent],
+  declarations: [
+	AppComponent,
+	GridColumnAutosizingComponent
+],
+  imports: [
+	BrowserModule,
+	BrowserAnimationsModule,
+	FormsModule,
+	IgxPreventDocumentScrollModule,
+	IgxGridModule
+],
+  providers: [],
+  entryComponents: [],
+  schemas: []
+})
+export class AppModule {}
