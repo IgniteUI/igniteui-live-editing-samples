59096f1a3f9e7f3d2ad5290a346991aa32ffe788
tfsbuild <tfsbuild@infragistics.com>
2022-09-05 14:23:09 -0400

    Commit submodule changes angular-samples-cd_2022.9.5.1

---
+++ angular-demos/grid/grid-validator-service/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppComponent } from "./app.component";
+import { GridValidatorServiceComponent } from "./grid/grid-validator-service/grid-validator-service.component";
+import { 
+	IgxGridModule,
+	IgxFocusModule
+ } from "igniteui-angular";
+import { IgxPreventDocumentScrollModule } from "./directives/prevent-scroll.directive";
+
+
+
+@NgModule({
+  bootstrap: [AppComponent],
+  declarations: [
+	AppComponent,
+	GridValidatorServiceComponent
+],
+  imports: [
+	BrowserModule,
+	BrowserAnimationsModule,
+	FormsModule,
+	IgxPreventDocumentScrollModule,
+	IgxGridModule,
+	IgxFocusModule
+],
+  providers: [],
+  entryComponents: [],
+  schemas: []
+})
+export class AppModule {}
