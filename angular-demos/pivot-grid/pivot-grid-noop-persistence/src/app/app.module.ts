0cd4b04e5209cd8bf0174900da14df66e2354f58
tfsbuild <tfsbuild@infragistics.com>
2022-09-09 07:39:44 -0400

    Commit submodule changes angular-samples-cd_2022.9.9.1

---
+++ angular-demos/pivot-grid/pivot-grid-noop-persistence/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppComponent } from "./app.component";
+import { IgxPivotGridModule } from "igniteui-angular";
+import { PivotGridNoopPersistenceSampleComponent } from "./pivot-grid/pivot-grid-noop-persistence/pivot-grid-noop-persistence-sample.component";
+import { IgxPreventDocumentScrollModule } from "./directives/prevent-scroll.directive";
+import { PivotDataService } from "./services/pivotRemoteData.service";
+
+
+
+@NgModule({
+  bootstrap: [AppComponent],
+  declarations: [
+	AppComponent,
+	PivotGridNoopPersistenceSampleComponent
+],
+  imports: [
+	BrowserModule,
+	BrowserAnimationsModule,
+	FormsModule,
+	IgxPreventDocumentScrollModule,
+	IgxPivotGridModule
+],
+  providers: [PivotDataService],
+  entryComponents: [],
+  schemas: []
+})
+export class AppModule {}
