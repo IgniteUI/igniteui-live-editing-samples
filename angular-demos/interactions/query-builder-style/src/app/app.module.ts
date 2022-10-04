8feee5a952fe2e6cc29b4dbdee36c9b4f7ed2246
tfsbuild <tfsbuild@infragistics.com>
2022-10-04 07:46:09 -0400

    Commit submodule changes angular-samples-cd_2022.10.4.1

---
+++ angular-demos/interactions/query-builder-style/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppComponent } from "./app.component";
+import { IgxQueryBuilderModule } from "igniteui-angular";
+import { QueryBuilderStyleComponent } from "./query-builder-style/query-builder-style.component";
+
+
+
+@NgModule({
+  bootstrap: [AppComponent],
+  declarations: [
+	AppComponent,
+	QueryBuilderStyleComponent
+],
+  imports: [
+	BrowserModule,
+	BrowserAnimationsModule,
+	FormsModule,
+	IgxQueryBuilderModule
+],
+  providers: [],
+  entryComponents: [],
+  schemas: []
+})
+export class AppModule {}
