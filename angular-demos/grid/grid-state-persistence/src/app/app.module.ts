eeebc5c8c6da719f6af65c725650e3c407b40bdb
tfsbuild <tfsbuild@infragistics.com>
2023-01-05 17:02:58 +0200

    Commit submodule changes angular-samples-cd_2023.1.5.5

---
+++ angular-demos/grid/grid-state-persistence/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppComponent } from "./app.component";
+import { GridStatePersistenceSampleComponent } from "./grid/grid-state-persistence-sample/grid-state-persistance-sample.component";
+import { 
+	IgxAvatarModule,
+	IgxGridModule,
+	IgxSwitchModule,
+	IgxExcelExporterService,
+	IgxCsvExporterService
+ } from "igniteui-angular";
+import { IgxPreventDocumentScrollModule } from "./directives/prevent-scroll.directive";
+import { FinancialDataService } from "./services/financial.service";
+
+
+
+@NgModule({
+  bootstrap: [AppComponent],
+  declarations: [
+	AppComponent,
+	GridStatePersistenceSampleComponent
+],
+  imports: [
+	BrowserModule,
+	BrowserAnimationsModule,
+	FormsModule,
+	IgxPreventDocumentScrollModule,
+	IgxAvatarModule,
+	IgxGridModule,
+	IgxSwitchModule
+],
+  providers: [FinancialDataService],
+  entryComponents: [],
+  schemas: []
+})
+export class AppModule {}
