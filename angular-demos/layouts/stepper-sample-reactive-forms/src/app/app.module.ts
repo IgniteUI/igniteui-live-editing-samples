1fef81b3a779cf1a39a41e096a152b2bc619b657
tfsbuild <tfsbuild@infragistics.com>
2024-05-31 19:16:52 +0300

    Commit submodule changes angular-samples-cd_2024.5.31.6

---
+++ angular-demos/layouts/stepper-sample-reactive-forms/src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppComponent } from "./app.component";
+import { StepperSampleReactiveFormsComponent } from "./stepper-sample-reactive-forms/stepper-sample-reactive-forms.component";
+import { 
+	IgxStepperModule,
+	IgxMaskModule,
+	IgxInputGroupModule,
+	IgxButtonModule,
+	IgxRadioModule,
+	IgxCardModule,
+	IgxCheckboxModule,
+	IgxSelectModule,
+	IgxIconModule,
+	IgxBadgeModule
+ } from "igniteui-angular";
+
+
+
+@NgModule({
+  bootstrap: [AppComponent],
+  declarations: [
+	AppComponent,
+	StepperSampleReactiveFormsComponent
+],
+  imports: [
+	BrowserModule,
+	BrowserAnimationsModule,
+	FormsModule,
+	IgxStepperModule,
+	IgxMaskModule,
+	IgxInputGroupModule,
+	IgxButtonModule,
+	IgxRadioModule,
+	IgxCardModule,
+	IgxCheckboxModule,
+	IgxSelectModule,
+	IgxIconModule,
+	IgxBadgeModule
+],
+  providers: [],
+  schemas: []
+})
+export class AppModule {}
