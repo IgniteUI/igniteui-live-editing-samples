af3d5158c0b62ce984b698b22313de0eef6fe8ae
tfsbuild <tfsbuild@infragistics.com>
2025-09-25 14:04:05 +0000

    Automated repository update

---
+++ angular-demos/hierarchical-grid/hierarchical-grid-cell-merge-custom/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+import { HGridCellMergeCustomComponent } from "./hierarchical-grid/hierarchical-grid-cell-merge-custom/hierarchical-grid-cell-merge-custom.component";
+
+@Component({
+    selector: "app-root",
+    styleUrls: ["./app.component.scss"],
+    templateUrl: "./app.component.html",
+    imports: [HGridCellMergeCustomComponent]
+})
+export class AppComponent {}
---
+++ angular-demos/tree-grid/treegrid-cell-merge-custom/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+import { TreeGridCellMergeCustomComponent } from "./tree-grid/tree-grid-cell-merge-custom-sample/tree-grid-cell-merge-custom-sample.component";
+
+@Component({
+    selector: "app-root",
+    styleUrls: ["./app.component.scss"],
+    templateUrl: "./app.component.html",
+    imports: [TreeGridCellMergeCustomComponent]
+})
+export class AppComponent {}
