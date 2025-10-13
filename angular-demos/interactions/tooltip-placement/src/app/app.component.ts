26bdd88089807813158fc2d4802a6e62b4230dec
tfsbuild <tfsbuild@infragistics.com>
2025-10-13 12:01:43 +0000

    Automated repository update

---
+++ angular-demos/interactions/tooltip-placement/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+import { TooltipPlacementComponent } from "./tooltip-placement/tooltip-placement.component";
+
+@Component({
+    selector: "app-root",
+    styleUrls: ["./app.component.scss"],
+    templateUrl: "./app.component.html",
+    imports: [TooltipPlacementComponent]
+})
+export class AppComponent {}
